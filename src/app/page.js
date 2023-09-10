af605e72de833359bed954df62bbc3a96aab1a72
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-10 10:05:38 +0530

    data pass using props

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,18 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <User></User>
+      <User name = "Swarup"></User>
+      <User name = "Nextjs"></User>
       <h1>Home Page</h1>
     </main>
   )
 }
 
 
-const User=() => {
+const User=(props) => {
   return(
     <div>
-      <h2>User name is Swarup</h2>
+      <h2>User name is {props.name}</h2>
     </div>
   )
 }
