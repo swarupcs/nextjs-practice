8eee4ef661f3b16125ef8bf107ea0d6d83b3aca6
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-11 09:07:06 +0530

    Function call by data passing

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@
 import styles from './page.module.css'
 
 export default function Home() {
-  const apple=() => {
-    alert("Fruit");
+  const apple=(item) => {
+    alert(item);
   }
   return (
     <main className={styles.main}>
       <h1>Home Page</h1>
-      <button onClick={(apple)}>Click Me</button>
+      <button onClick={()=>apple("Fruit")}>Click Me</button>
     </main>
   )
 }
