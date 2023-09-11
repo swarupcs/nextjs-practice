d1929a61fb12acba2b6a663553e9c01ba49f2777
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-11 09:14:05 +0530

    useState in nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
 'use client'
+import { useState } from 'react';
 import styles from './page.module.css'
 
 export default function Home() {
-  const apple=(item) => {
-    alert(item);
+  const [name, setName] = useState("Swarup");
+  const apple=() => {
+    setName("Nexjs");
   }
   return (
     <main className={styles.main}>
-      <h1>Home Page</h1>
-      <button onClick={()=>apple("Fruit")}>Click Me</button>
+      <h1>Home Page {name}</h1>
+      <button onClick={()=>apple()}>Click Me</button>
     </main>
   )
 }
