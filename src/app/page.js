d00a9118596271fa7f62549ee7f85c83456fbfd5
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-11 08:52:45 +0530

    use client

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,13 @@
+'use client'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <User name = "Swarup"></User>
-      <User name = "Nextjs"></User>
       <h1>Home Page</h1>
+      <button onClick={()=> alert("Hello next js")}>Click Me</button>
     </main>
   )
 }
 
 
-const User=(props) => {
-  return(
-    <div>
-      <h2>User name is {props.name}</h2>
-    </div>
-  )
-}
