99b938b133bf8f64ac9b4133391f3d56ab994a4a
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-11 09:05:54 +0530

    call function

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,13 @@
 import styles from './page.module.css'
 
 export default function Home() {
+  const apple=() => {
+    alert("Fruit");
+  }
   return (
     <main className={styles.main}>
       <h1>Home Page</h1>
-      <button onClick={()=> alert("Hello next js")}>Click Me</button>
+      <button onClick={(apple)}>Click Me</button>
     </main>
   )
 }
