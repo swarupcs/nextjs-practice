c1d1031033284cf5adb8b74f35ff7c60d2e0e0fa
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-15 09:27:24 +0530

    Middleware - 1

---
+++ src/middleware.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+
+    if(request.nextUrl.pathname!="/login") {
+        return NextResponse.redirect(new URL("/login",request.url))
+    }
+}
