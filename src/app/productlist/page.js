02cc4e926239d5e7f0d9de4911c0bd90a41320bc
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-15 10:51:13 +0530

    Call API in Server Component

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,23 +1,19 @@
-"use client"
-import { useEffect, useState } from "react"
+async function productlist() {
+    let data = await fetch("https://dummyjson.com/products");
+    data = await data.json();
+    return data.products
+}
 
-export default function Page() {
-    const [product, setProduct] = useState([])
-    useEffect(async() => {
-      
-      let data = await fetch("https://dummyjson.com/products");
-      data = await data.json();
-    //   console.log(product);
-      setProduct(data.products);  
-    }, [])
-    
-
+export default async function Page() {
+    let products = await productlist()
+    console.log(products);
     return(
         <div>
-            <h1>Product List</h1>
-            {
-                product.map((item)=> (
-                    <h3>Name : {item.title} , Price : {item.price}</h3>
+            <h1>Product List</h1> {
+                products.map((item)=> (
+                    <div>
+                        <h3>Name : {item.title}</h3>
+                    </div>
                 ))
             }
         </div>
