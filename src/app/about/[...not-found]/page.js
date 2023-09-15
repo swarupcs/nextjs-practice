7947625bc9db3f4c65ea29140efa2f4408cc435d
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-15 08:49:57 +0530

    404 Page in nextjs

---
+++ src/app/about/[...not-found]/page.js
@@ -0,0 +1,7 @@
+export default function AboutNotFound(){
+    return(
+        <div>
+            <h1>This About Page is not avaiable</h1>
+        </div>
+    )
+}
---
+++ src/app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound(){
+    return(
+        <div>
+            <h1>This Page is not avaiable</h1>
+        </div>
+    )
+}
