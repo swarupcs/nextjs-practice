543a0c9b921d00e6e284f9f24d7dca2b03807665
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-15 18:47:55 +0530

    global css

--- src/app/page.js
+++ src/app/page.js
@@ -22,8 +22,11 @@ export default function Home() {
       <button onClick={()=> navigate("/login")} >Go to Login Page</button>
       <button onClick={()=> navigate("/about")} >Go to About Page</button> */}
 
-      <h1>Fetch Data with API in Client Component</h1>
-      <Link href="/productlist">Go to Product List</Link>
+      {/* <h1>Fetch Data with API in Client Component</h1>
+      <Link href="/productlist">Go to Product List</Link> */}
+
+      <h1>Styles and CSS in NEXTJS</h1>
+      <h2>Heading 2 in main page</h2>
     </main>
   )
 }
---
+++ src/app/user/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return(
+        <div>
+            <h1>User Page</h1>
+            <h2>Heading 2 in user page</h2>
+        </div>
+    )
+}
