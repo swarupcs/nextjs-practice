f95e1b6e674eda8ab24e3cc79073931ac9e88af4
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-15 09:37:32 +0530

    middleware-2

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,11 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
 
-    if(request.nextUrl.pathname!="/login") {
+    // if(request.nextUrl.pathname!="/login") {
         return NextResponse.redirect(new URL("/login",request.url))
-    }
+    // }
+}
+
+export const config = {
+    matcher : ["/about/:path*", "/studentlist/:path*"]
 }
