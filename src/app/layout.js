4f2ef16de0e70337a6ee05fa0ed105a532b6d5e8
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-15 19:44:45 +0530

    Conditional Style and CSS in Nextjs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-import './styles.css'
+// import './styles.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,20 @@ import Link from 'next/link';
 import styles from './page.module.css'
 import { useRouter } from 'next/navigation';
 
-import custom from './custom.module.css'
-import other from './other.module.css'
-import outside from '@/style/outside.module.css'
+// import custom from './custom.module.css'
+// import other from './other.module.css'
+// import outside from '@/style/outside.module.css'
+
+import style from "./style.module.css"
 
 export default function Home() {
-  const router = useRouter();
-  const navigate=(name)=> {
-    router.push(name);
-  }
+  // const router = useRouter();
+  // const navigate=(name)=> {
+  //   router.push(name);
+  // }
+
+  const [color, setColor] = useState("red")
+  const {red} = style
 
   return (
     <main>
@@ -32,9 +37,24 @@ export default function Home() {
       {/* <h1>Styles and CSS in NEXTJS</h1>
       <h2>Heading 2 in main page</h2> */}
 
-      <h1 className={custom.main}>CSS Modules with Next js</h1>
+      {/* <h1 className={custom.main}>CSS Modules with Next js</h1>
       <h2 className={other.main}>CSS Modules with Next js</h2>
-      <p className={outside.main}>Some Text</p>
+      <p className={outside.main}>Some Text</p> */}
+
+
+
+      <h1 className={color=="red" ? style.red : style.green}>Condition with Style</h1>
+      <h2 style={{backgroundColor:color=='red' ? 'red' : 'green'}}>Heading 2</h2>
+      <h3 id={style.orange}>Heading 3</h3>
+
+      <h4 className={red}>Dummy Text</h4>
+      <h4 className={red}>Dummy Text</h4>
+      <h4 className={red}>Dummy Text</h4>
+      <h4 className={red}>Dummy Text</h4>
+      <h4 className={red}>Dummy Text</h4>
+
+
+      <button onClick={()=> setColor("green")}>Update Color</button>
 
     </main>
   )
